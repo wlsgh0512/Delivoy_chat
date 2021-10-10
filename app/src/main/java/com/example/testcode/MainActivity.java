40c166c48520f151be48dc7917381ced1f9a4621
so <socomee@naver.com>
2021-10-11 08:39:43 +0900

    View Binding을 적용한다

--- app/src/main/java/com/example/testcode/MainActivity.java
+++ app/src/main/java/com/example/testcode/MainActivity.java
@@ -17,6 +17,7 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.testcode.databinding.ActivityMainBinding;
 import com.example.testcode.model.ErrorDto;
 import com.example.testcode.model.LoginResponse;
 import com.example.testcode.model.Login_id_Response;
@@ -54,47 +55,27 @@ public class MainActivity extends AppCompatActivity {
     String me = "";
     String id = "";
     String pw = "";
-    Button btn_login;
-    TextView btnFindUserId, btnFindUserPw, join_member;
-    EditText user_id, user_pw;
-    ImageButton btn_find;
-    RadioButton rb_old_login, rb_user_id_login;
-    RadioGroup rg_login_method;
-    LinearLayout ll_old_user_id_wrap;
-    AppCompatEditText edtUserAreaNo, edtUserDistribId, edtUserAgencyId, edtUserCourId;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_login = (Button) findViewById(R.id.btn_login);
-        btnFindUserId = (TextView) findViewById(R.id.btnFindUserId);
-        btnFindUserPw = (TextView) findViewById(R.id.btnFindUserPw);
-        join_member = (TextView) findViewById(R.id.join_member);
-        user_id = (EditText) findViewById(R.id.user_id);
-        user_pw = (EditText) findViewById(R.id.user_pw);
-        btn_find = (ImageButton) findViewById(R.id.btn_find);
-        rb_old_login = (RadioButton) findViewById(R.id.rb_old_login);
-        rb_user_id_login = (RadioButton) findViewById(R.id.rb_user_id_login);
-        rg_login_method = (RadioGroup) findViewById(R.id.rg_login_method);
-        ll_old_user_id_wrap = (LinearLayout) findViewById(R.id.ll_old_user_id_wrap);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        edtUserAreaNo = (AppCompatEditText) findViewById(R.id.edtUserAreaNo);
-        edtUserDistribId = (AppCompatEditText) findViewById(R.id.edtUserDistribId);
-        edtUserAgencyId = (AppCompatEditText) findViewById(R.id.edtUserAgencyId);
-        edtUserCourId = (AppCompatEditText) findViewById(R.id.edtUserCourId);
-
-        rg_login_method.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int id) {
-                RadioButton select = (RadioButton) findViewById(id);
-                if (select == rb_old_login) {
-                    ll_old_user_id_wrap.setVisibility(View.VISIBLE);
-                    user_id.setVisibility(View.INVISIBLE);
-                } else if (select == rb_user_id_login) {
-                    ll_old_user_id_wrap.setVisibility(View.INVISIBLE);
-                    user_id.setVisibility(View.VISIBLE);
+        binding.rgLoginMethod.setOnCheckedChangeListener((radioGroup, id) -> {
+            switch (radioGroup.getCheckedRadioButtonId()) {
+                case R.id.rb_old_login: {
+                    binding.llOldUserIdWrap.setVisibility(View.VISIBLE);
+                    binding.userId.setVisibility(View.INVISIBLE);
+                    break;
+                }
+                case R.id.rb_user_id_login: {
+                    binding.llOldUserIdWrap.setVisibility(View.INVISIBLE);
+                    binding.userId.setVisibility(View.VISIBLE);
+                    break;
                 }
             }
         });
@@ -104,8 +85,7 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()) {
             // 로그인 버튼을 눌렀을 때
             case R.id.btn_login:
-                RadioGroup radioGroup = findViewById(R.id.rg_login_method);
-                switch (radioGroup.getCheckedRadioButtonId()) {
+                switch (binding.rgLoginMethod.getCheckedRadioButtonId()) {
                     case R.id.rb_old_login:
                         getOldLogin();
                         break;
@@ -139,11 +119,16 @@ public class MainActivity extends AppCompatActivity {
             String url = String.format("http://%s/chatt/app/login/login_no_get.php", hostname);
 
             HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder();
-            urlBuilder.addQueryParameter("ucAreaNo", ((TextView) findViewById(R.id.edtUserAreaNo)).getText().toString());
-            urlBuilder.addQueryParameter("ucDistribId", ((TextView) findViewById(R.id.edtUserDistribId)).getText().toString());
-            urlBuilder.addQueryParameter("ucAgencyId", ((TextView) findViewById(R.id.edtUserAgencyId)).getText().toString());
-            urlBuilder.addQueryParameter("ucMemCourId", ((TextView) findViewById(R.id.edtUserCourId)).getText().toString());
-            urlBuilder.addQueryParameter("acPassword", ((TextView) findViewById(R.id.user_pw)).getText().toString());
+            urlBuilder.addQueryParameter("ucAreaNo",
+                    binding.edtUserAreaNo.getText().toString());
+            urlBuilder.addQueryParameter("ucDistribId",
+                    binding.edtUserDistribId.getText().toString());
+            urlBuilder.addQueryParameter("ucAgencyId",
+                    binding.edtUserAgencyId.getText().toString());
+            urlBuilder.addQueryParameter("ucMemCourId",
+                    binding.edtUserCourId.getText().toString());
+            urlBuilder.addQueryParameter("acPassword",
+                    binding.userPw.getText().toString());
 
             Request request = new Request.Builder()
                     .url(urlBuilder.build().toString())
@@ -159,31 +144,43 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onResponse(Call call, final Response response) throws IOException {
                     if (!response.isSuccessful()) {
-                        final ErrorDto error = new Gson().fromJson(response.body().string(), ErrorDto.class);
+                        final ErrorDto error = new Gson().fromJson(response.body().string(),
+                                ErrorDto.class);
                         Log.i("tag", error.message);
                         // 응답 실패
                         Log.i("tag", "응답실패");
                     } else {
-                        ar = edtUserAreaNo.getText().toString();
-                        di = edtUserDistribId.getText().toString();
-                        ag = edtUserAgencyId.getText().toString();
-                        me = edtUserCourId.getText().toString();
+                        ar = binding.edtUserAreaNo.getText().toString();
+                        di = binding.edtUserDistribId.getText().toString();
+                        ag = binding.edtUserAgencyId.getText().toString();
+                        me = binding.edtUserCourId.getText().toString();
                         oldid = ar + di + ag + me;
 
                         // 응답 성공
                         Log.i("tag", "응답 성공");
                         final String responseData = response.body().string();
-                        final LoginResponse loginResponse = new Gson().fromJson(responseData, LoginResponse.class);
+                        final LoginResponse loginResponse = new Gson().fromJson(responseData,
+                                LoginResponse.class);
                         runOnUiThread(() -> {
                             try {
                                 // 아이디 비밀번호 일치 시 로그인 성공하게 하는 조건?
-//                                if (oldid.equals(loginResponse.ucAreaNo + loginResponse.ucDistribId + loginResponse.ucAgencyId + loginResponse.ucMemCourId)) {
-//                                    Intent intent = new Intent(MainActivity.this, User_Consent.class);
-//                                    startActivity(intent);
-//                                    Toast.makeText(getApplicationContext(), "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show();
-//                                }
+                                //                                if (oldid.equals(loginResponse
+                                //                                .ucAreaNo + loginResponse
+                                //                                .ucDistribId + loginResponse
+                                //                                .ucAgencyId + loginResponse
+                                //                                .ucMemCourId)) {
+                                //                                    Intent intent = new Intent
+                                //                                    (MainActivity.this,
+                                //                                    User_Consent.class);
+                                //                                    startActivity(intent);
+                                //                                    Toast.makeText
+                                //                                    (getApplicationContext(),
+                                //                                    "로그인에 성공했습니다.", Toast
+                                //                                    .LENGTH_SHORT).show();
+                                //                                }
 
-                                SharedPreferences sharedPreferences = getSharedPreferences("test", MODE_PRIVATE);
+                                SharedPreferences sharedPreferences = getSharedPreferences("test"
+                                        , MODE_PRIVATE);
                                 SharedPreferences.Editor editor = sharedPreferences.edit();
                                 editor.putString("ar", loginResponse.ucAreaNo);
                                 editor.putString("di", loginResponse.ucDistribId);
@@ -197,14 +194,18 @@ public class MainActivity extends AppCompatActivity {
 
                                 // Option 두 개의 Value값이 1이라면 이미 동의를 한 것.
                                 // 약관 동의를 마치면 Option이 1이 되게 하여 약관 동의 페이지를 생략하도록.
-                                if(loginResponse.ucAgreeOption.equals("1") && loginResponse.ucThirdPartyOption.equals("1") ) {
-                                    Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                                if (loginResponse.ucAgreeOption.equals("1") && loginResponse.ucThirdPartyOption.equals("1")) {
+                                    Intent intent = new Intent(MainActivity.this,
+                                            ListActivity.class);
                                     startActivity(intent);
-                                    Toast.makeText(getApplicationContext(), "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getApplicationContext(), "로그인에 성공했습니다.",
+                                            Toast.LENGTH_SHORT).show();
                                 } else {
-                                    Intent intent = new Intent(MainActivity.this, User_Consent.class);
+                                    Intent intent = new Intent(MainActivity.this,
+                                            User_Consent.class);
                                     startActivity(intent);
-                                    Toast.makeText(getApplicationContext(), "첫 로그인 시 약관 동의가 필요합니다.", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getApplicationContext(), "첫 로그인 시 약관 동의가 필요합니다" +
+                                            ".", Toast.LENGTH_SHORT).show();
                                 }
                             } catch (Exception e) {
                                 e.printStackTrace();
@@ -225,8 +226,10 @@ public class MainActivity extends AppCompatActivity {
             String url = String.format("http://%s/chatt/app/login/login_id_get.php", hostname);
 
             HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder();
-            urlBuilder.addQueryParameter("acUserId", ((TextView) findViewById(R.id.user_id)).getText().toString());
-            urlBuilder.addQueryParameter("acPassword", ((TextView) findViewById(R.id.user_pw)).getText().toString());
+            urlBuilder.addQueryParameter("acUserId",
+                    binding.userId.getText().toString());
+            urlBuilder.addQueryParameter("acPassword",
+                    binding.userPw.getText().toString());
 
 
             Request request = new Request.Builder()
@@ -243,26 +246,32 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onResponse(Call call, final Response response) throws IOException {
                     if (!response.isSuccessful()) {
-                        final ErrorDto error = new Gson().fromJson(response.body().string(), ErrorDto.class);
+                        final ErrorDto error = new Gson().fromJson(response.body().string(),
+                                ErrorDto.class);
                         Log.i("tag", error.message);
                         // 응답 실패
                         Log.i("tag", "응답실패");
                     } else {
-                        id = user_id.getText().toString();
-                        pw = user_pw.getText().toString();
+                        id = binding.userId.getText().toString();
+                        pw = binding.userPw.getText().toString();
                         // 응답 성공
                         Log.i("tag", "응답 성공");
                         final String responseData = response.body().string();
-                        final LoginResponse login_id_Response = new Gson().fromJson(responseData, LoginResponse.class);
+                        final LoginResponse login_id_Response = new Gson().fromJson(responseData,
+                                LoginResponse.class);
                         runOnUiThread(() -> {
                             try {
-                                if(id.equals(login_id_Response.acUserId)) {
-                                    Intent intent = new Intent(MainActivity.this, User_Consent.class);
+                                if (id.equals(login_id_Response.acUserId)) {
+                                    Intent intent = new Intent(MainActivity.this,
+                                            User_Consent.class);
                                     startActivity(intent);
                                 }
-////                                    Toast.makeText(getApplicationContext(), "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show();
+                                ////                                    Toast.makeText
+                                // (getApplicationContext(), "로그인에 성공했습니다.", Toast.LENGTH_SHORT)
+                                // .show();
 
-                                Toast.makeText(getApplicationContext(), "" + login_id_Response.ucAreaNo, Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getApplicationContext(),
+                                        "" + login_id_Response.ucAreaNo, Toast.LENGTH_SHORT).show();
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
