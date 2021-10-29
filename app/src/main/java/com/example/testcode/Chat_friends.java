03777e160037cb6abe0715186c2925cb795e2199
wlsgh0512 <82078193+wlsgh0512@users.noreply.github.com>
2021-10-29 15:23:00 +0900

    onPause, onResume에 맞춰 타이머 동작하게 구현

--- app/src/main/java/com/example/testcode/Chat_friends.java
+++ app/src/main/java/com/example/testcode/Chat_friends.java
@@ -12,25 +12,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
 
 import com.example.testcode.api.LoginService;
 import com.example.testcode.config.RetrofitConfig;
 import com.example.testcode.databinding.ActivityChatFriendsBinding;
-import com.example.testcode.databinding.ActivityJoinMemberBinding;
 import com.example.testcode.model.Chat_Response;
 import com.example.testcode.model.Code;
 import com.example.testcode.model.DataItem;
+import com.example.testcode.model.DataItem2;
 import com.example.testcode.model.ErrorDto;
-import com.example.testcode.model.FriendsResponse;
-import com.example.testcode.model.Join_Response;
 import com.example.testcode.model.MyAdapter;
 import com.example.testcode.model.PollingChat_Response;
 import com.example.testcode.model.Send_msg_Response;
@@ -38,36 +34,25 @@ import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * 채팅창 화면.
- * response.body.string() 값 잘 받아오는것까지는 확인.
- * ArrayList에 add해서 채팅창에 갱신 확인.
- * 나오긴 하는데 화면에 진입했을 때 딱 안나오고 EditText를 한번 눌러야 나와서 (반응이 늦어서?)
- * 추후 작업 필요.
- * EditText를 눌렀을 때 마지막 채팅에 포커스가 맞춰지도록 .
- *
- * error code 500
- * talk_post = Expected BEGIN_OBJECT but was STRING at line 8 column 1 path $
- * 객체가 올것으로 기대 했는데 실제 데이터가 STRIN?
  */
 
 public class Chat_friends extends AppCompatActivity {
     String msg = "";
     ArrayList<DataItem> dataList = new ArrayList<>();
-    int getPosition;
     MyAdapter adapter;
     ActionBar actionBar;
+    List<PollingChat_Response.Items.Rooms> items;
+    Timer timer;
+    Handler handler;
 
-    String ucAreaNo, ucDistribId, ucAgencyId, ucMemCourId, name, getUiRoomNo, testUiRoomNo;
+    String ucAreaNo, ucDistribId, ucAgencyId, ucMemCourId, name, getUiRoomNo, testUiRoomNo, uiTalkNo;
 
     ActivityChatFriendsBinding binding;
 
@@ -78,16 +63,11 @@ public class Chat_friends extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         actionBar = getSupportActionBar();  //제목줄 객체 얻어오기
-
         msg = binding.sendMsg.getText().toString();
 
 //        get_talk();
 
-        getPosition = getIntent().getIntExtra("uiRoomNo",1);
         testUiRoomNo = getIntent().getStringExtra("uiRoomNo");
-        getUiRoomNo = Integer.toString(getPosition);
-
-        get_polling_talk();
 
         LinearLayoutManager manager
                 = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
@@ -106,12 +86,21 @@ public class Chat_friends extends AppCompatActivity {
         ucMemCourId = sharedPreferences.getString("me","");
         name = sharedPreferences.getString("name","");
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                binding.recyvlerv.scrollToPosition(dataList.size() - 1);
-            }
-        }, 200);
+
+//        timer = new Timer(true); //인자가 Daemon 설정인데 true 여야 죽지 않음.
+//        handler = new Handler();
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                handler.post(new Runnable(){
+//                    public void run(){
+//                        get_polling_talk();
+//                        binding.recyvlerv.scrollToPosition(dataList.size() - 1);
+//                    }
+//                });
+//            }
+//        }, 0, 100);
+
     }
 
     @Override
@@ -131,8 +120,15 @@ public class Chat_friends extends AppCompatActivity {
              * 대화상대 초대.
              * EditText 밑으로 친구 목록 리스트업.
              */
+            case R.id.list:
+                Intent intent0 = new Intent(Chat_friends.this, ListTest.class);
+                intent0.putExtra("RoomNo", testUiRoomNo);
+                startActivity(intent0);
+                break;
+
             case R.id.invite:
                 Intent intent = new Intent(Chat_friends.this, Chat_invite_room.class);
+                intent.putExtra("RoomNo", testUiRoomNo);
                 startActivity(intent);
                 break;
 
@@ -190,7 +186,8 @@ public class Chat_friends extends AppCompatActivity {
         try {
             final String message = binding.sendMsg.getText().toString();
             LoginService service = (new RetrofitConfig()).getRetrofit().create(LoginService.class);
-            service.send_msg("1",
+            service.send_msg(
+                    testUiRoomNo,
                     ucAreaNo,
                     ucDistribId,
                     ucAgencyId,
@@ -204,11 +201,19 @@ public class Chat_friends extends AppCompatActivity {
                                 // 응답 성공
                                 Log.i("tag", "응답 성공");
                                 try {
-                                    //final Send_msg_Response send_msg_response = response.body();
+                                    final Send_msg_Response send_msg_response = response.body();
 
-                                    dataList.add(new DataItem(message, "", Code.ViewType.RIGHT_CONTENT));
+                                    dataList.add(new DataItem(
+                                            send_msg_response.uiTalkNo,
+                                            message,
+                                            "", Code.ViewType.RIGHT_CONTENT));
                                     binding.recyvlerv.getAdapter().notifyDataSetChanged();
                                     adapter.notifyDataSetChanged();
+
+//                                    SharedPreferences sp = getPreferences(MODE_PRIVATE);
+//                                    SharedPreferences.Editor editor = sp.edit();
+//                                    editor.putString("TalkNo", String.valueOf(send_msg_response.uiTalkNo));
+//                                    editor.commit();
 
                                 } catch (Exception e) {
                                     e.printStackTrace();
@@ -240,70 +245,72 @@ public class Chat_friends extends AppCompatActivity {
     }
 
 
-    public void get_talk() {
-        try {
-            LoginService service = (new RetrofitConfig()).getRetrofit().create(LoginService.class);
-            service.chat("1",
-                    getUiRoomNo,
-                    ucAreaNo,
-                    ucDistribId,
-                    ucAgencyId,
-                    ucMemCourId)
-                    .enqueue(new retrofit2.Callback<Chat_Response>() {
-                        @Override
-                        public void onResponse(retrofit2.Call<Chat_Response> call,
-                                               retrofit2.Response<Chat_Response> response) {
-                            if (response.isSuccessful()) {
-                                // 응답 성공
-                                Log.i("tag", "응답 성공");
-                                try {
-                                    final Chat_Response chat_response = response.body();
-
-
-                                    for (int i = 0; i < chat_response.uiTalkNo.length(); i ++) {
-                                        if (ucMemCourId != chat_response.ucMemCourId) {
-                                            dataList.add(new DataItem(chat_response.acTalkMesg, chat_response.acRealName, Code.ViewType.LEFT_CONTENT));
-                                            actionBar.setTitle(chat_response.acRealName);
-                                        }
-                                    }
-
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                            } else {
-                                final ErrorDto error;
-                                try {
-                                    error = new Gson().fromJson(response.errorBody().string(),
-                                            ErrorDto.class);
-                                    Log.i("tag", error.message);
-                                    // 응답 실패
-                                    Log.i("tag", "응답실패");
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-
-                        }
-
-                        @Override
-                        public void onFailure(retrofit2.Call<Chat_Response> call, Throwable t) {
-
-                        }
-                    });
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
+//    public void get_talk() {
+//        try {
+//            LoginService service = (new RetrofitConfig()).getRetrofit().create(LoginService.class);
+//            service.chat(adapter.getLastItem().getTalkId(),
+//                    testUiRoomNo,
+//                    ucAreaNo,
+//                    ucDistribId,
+//                    ucAgencyId,
+//                    ucMemCourId)
+//                    .enqueue(new retrofit2.Callback<Chat_Response>() {
+//                        @Override
+//                        public void onResponse(retrofit2.Call<Chat_Response> call,
+//                                               retrofit2.Response<Chat_Response> response) {
+//                            if (response.isSuccessful()) {
+//                                // 응답 성공
+//                                Log.i("tag", "응답 성공");
+//                                try {
+//                                    final Chat_Response chat_response = response.body();
+//
+//                                    for (int i = 0; i < chat_response.uiTalkNo.length(); i ++) {
+//                                        if (ucMemCourId != chat_response.ucMemCourId) {
+//                                            dataList.add(new DataItem(chat_response.uiTalkNo, chat_response.acTalkMesg, chat_response.acRealName, Code.ViewType.LEFT_CONTENT));
+//                                            actionBar.setTitle(chat_response.acRealName);
+//                                        }
+//                                    }
+//
+//                                } catch (Exception e) {
+//                                    e.printStackTrace();
+//                                }
+//                            } else {
+//                                final ErrorDto error;
+//                                try {
+//                                    error = new Gson().fromJson(response.errorBody().string(),
+//                                            ErrorDto.class);
+//                                    Log.i("tag", error.message);
+//                                    // 응답 실패
+//                                    Log.i("tag", "응답실패");
+//                                } catch (IOException e) {
+//                                    e.printStackTrace();
+//                                }
+//                            }
+//
+//                        }
+//
+//                        @Override
+//                        public void onFailure(retrofit2.Call<Chat_Response> call, Throwable t) {
+//
+//                        }
+//                    });
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public void get_polling_talk() {
         try {
+//            SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);    // test 이름의 기본모드 설정, 만약 test key값이 있다면 해당 값을 불러옴.
+//            uiTalkNo = sharedPreferences.getString("TalkNo", "");
+
+//            final int uiTalkNo = adapter.getLastItem().getTalkId() -1 ;
 
             LoginService service = (new RetrofitConfig()).getRetrofit().create(LoginService.class);
-            service.pollingchat("1",
-//                    "9",
-//                    getUiRoomNo,
+            service.pollingchat(
+//                    uiTalkNo,
+                    0,
                     testUiRoomNo,
                     ucAreaNo,
                     ucDistribId,
@@ -319,15 +326,23 @@ public class Chat_friends extends AppCompatActivity {
                                 try {
                                     final PollingChat_Response pollingChat_response = response.body();
 
-                                    List<PollingChat_Response.Items.Rooms> items = pollingChat_response.items.astRooms;
+                                     items = pollingChat_response.items.astRooms;
 
-                                    for(int i = 0 ; i < items.size(); i++) {
-                                        if( !ucMemCourId.equals(pollingChat_response.items.astRooms.get(i).ucMemCourId)) {
-                                            dataList.add(new DataItem(items.get(i).acTalkMesg, items.get(i).acRealName, Code.ViewType.LEFT_CONTENT));
+                                    for (int i = 0; i < items.size(); i++) {
+                                        if (!ucMemCourId.equals(pollingChat_response.items.astRooms.get(i).ucMemCourId)) {
+                                            // dataList (uiTalkNo, acTalkMesg, acRealName, ViewType.CONTENT)
+                                            dataList.add(new DataItem(
+                                                    items.get(i).uiTalkNo,
+                                                    items.get(i).acTalkMesg,
+                                                    items.get(i).acRealName, Code.ViewType.LEFT_CONTENT));
                                         } else {
-                                            dataList.add(new DataItem(items.get(i).acTalkMesg, name, Code.ViewType.RIGHT_CONTENT));
+                                            dataList.add(new DataItem(
+                                                    items.get(i).uiTalkNo,
+                                                    items.get(i).acTalkMesg,
+                                                    name, Code.ViewType.RIGHT_CONTENT));
                                         }
                                     }
+
 
                                 } catch (Exception e) {
                                     e.printStackTrace();
@@ -358,11 +373,33 @@ public class Chat_friends extends AppCompatActivity {
         }
     }
 
-
-
-
-    public void initializeData() {
-        dataList = new ArrayList<>();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        timer.cancel();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        timer = new Timer(true); //인자가 Daemon 설정인데 true 여야 죽지 않음.
+        handler = new Handler();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable(){
+                    public void run(){
+                        get_polling_talk();
+                        binding.recyvlerv.scrollToPosition(dataList.size() - 1);
+                    }
+                });
+            }
+        }, 0, 100);
+    }
+
+
+
+
+
 
 }
